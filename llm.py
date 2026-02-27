15a33d9d1d11f1d7b56e867b550a1a5596d3969f
Vladimir Anyanov <v.s.anyanov@gmail.com>
2026-02-27 16:30:01 +0300

    Updated

--- llm.py
+++ llm.py
@@ -1,9 +1,17 @@
 import os
+from dotenv import load_dotenv
 from gigachat import GigaChat
 from langchain_gigachat.chat_models import GigaChat as LangGigaChat
 
-client = GigaChat(credentials=os.getenv("GIGACHAT_CREDENTIALS"), scope=os.getenv("GIGACHAT_SCOPE"))
+load_dotenv()
+
+client = GigaChat(
+    credentials=os.getenv("GIGACHAT_CREDENTIALS"),
+    scope=os.getenv("GIGACHAT_SCOPE"),
+    verify_ssl_certs=False,
+)
 
 llm = LangGigaChat(giga_chat=client)
+
 
 
