4c78590a439ad8b3002cece14c8d45646dc47882
Vladimir Anyanov <v.s.anyanov@gmail.com>
2026-02-27 15:59:49 +0300

    Created

---
+++ llm.py
@@ -0,0 +1,3 @@
+from gigachat import GigaChat
+
+giga = GigaChat(credentials="MDE5YjFiYmQtMzg0Mi03YzQyLWE5MDMtNDc0MWY5ODE0YjRmOmExOWY3NjI2LTE0YWQtNDMxNC04MzA5LTA1MmNhNWM5MjM1Zg==", scope="GIGACHAT_API_PERS")
