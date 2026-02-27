20551d1972f4e8d793ebaf50c0e65983aae780d1
Vladimir Anyanov <v.s.anyanov@gmail.com>
2026-02-27 16:11:26 +0300

    Updated

--- llm.py
+++ llm.py
@@ -1,10 +1,11 @@
 import os
 from dotenv import load_dotenv
+from langchain_gigachat import GigaChatLLM, ChatGigaChat
 from gigachat import GigaChat
 
-load_dotenv()  # Загружает .env автоматически
+load_dotenv()
 
-credentials = os.getenv("GIGACHAT_CREDENTIALS")
-scope = os.getenv("GIGACHAT_SCOPE")
+giga = GigaChat(credentials=os.getenv("GIGACHAT_CREDENTIALS"), scope=os.getenv("GIGACHAT_SCOPE"))
 
-giga = GigaChat(credentials=credentials, scope=scope)
+llm = ChatGigaChat(giga_chat=giga)  # Или GigaChatLLM(giga_chat=giga)
+
