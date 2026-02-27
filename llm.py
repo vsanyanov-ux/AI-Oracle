977ebce1b7d344cf94b1fc09a76fce54340d5d2c
Vladimir Anyanov <v.s.anyanov@gmail.com>
2026-02-27 16:18:07 +0300

    Without certificate

--- llm.py
+++ llm.py
@@ -1,12 +1,16 @@
 # llm.py
 import os
 from dotenv import load_dotenv
-from langchain_gigachat.chat_models import GigaChat  # Правильный импорт!
+from langchain_gigachat.chat_models import GigaChat
 from gigachat import GigaChat as GigaChatClient
 
 load_dotenv()
 
-client = GigaChatClient(credentials=os.getenv("GIGACHAT_CREDENTIALS"), scope=os.getenv("GIGACHAT_SCOPE"), verify_ssl_certs=False)
+client = GigaChatClient(
+    credentials=os.getenv("GIGACHAT_CREDENTIALS"),
+    scope=os.getenv("GIGACHAT_SCOPE"),
+    verify_ssl_certs=False  # Решение SSL-проблемы
+)
 
-llm = GigaChat(giga_chat=client)  # Chat модель для LangChain
+llm = GigaChat(giga_chat=client)
 
